406c111f940380529e4588fcdf03805ac11127e4
Leandro Cesar de Almeida <llmanutencao@bol.com.br>
2017-12-09 21:53:41 -0200

    exercicio 01

---
+++ src/Exercicios/Exercicio01.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercicios;
+
+/**
+ *
+ * @author Leandro
+ */
+public class Exercicio01 {
+    
+    public static void main(String[] args) {
+        
+        int[] vetA = new int[5];
+        int[] vetB = new int[5];
+        
+        vetA[0] = 1;
+        vetA[1] = 2;
+        vetA[2] = 3;
+        vetA[3] = 4;
+        vetA[4] = 5;
+        
+        for(int i = 0; i < vetA.length; i++){
+            vetB[i] = vetA[i];
+        }
+        
+        
+    }
+}
